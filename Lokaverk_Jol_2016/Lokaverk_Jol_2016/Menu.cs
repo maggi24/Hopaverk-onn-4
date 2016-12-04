6545b77ea7a2881b56e1fa1aa91e8f4aebe38caf
maggi24 <islfyrirmig@gmail.com>
2016-12-04 14:58:13 +0000

    Add files via upload

--- Lokaverk_Jol_2016/Lokaverk_Jol_2016/Menu.cs
+++ Lokaverk_Jol_2016/Lokaverk_Jol_2016/Menu.cs
@@ -8,9 +8,51 @@ namespace Lokaverk_Jol_2016
 {
     class Menu
     {
+        Character chara = new Character();
+
+        public bool leavring_menu = false;
+        private string pick;
+        public string Pick
+        {
+            get
+            {
+                return pick;
+            }
+            set
+            {
+                pick = value;
+            }
+        }
+
+        public void menu_display()
+        {
+            Console.WriteLine("inventory");
+            Console.WriteLine("stats");
+            Console.WriteLine("Exit");
+        }
+        public void menu()
+        {
+            switch (pick)
+            {
+                case "inventory":
+                    //inv
+                    Console.WriteLine("Invt");
+                    break;
+                case "stats":
+                    //chara.stats();
+                    Console.WriteLine("Stats");
+                    break;
+                case "exit":
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new ArgumentOutOfRangeException("\r\nYou have drop out off menu", pick, "That is not a value Hint  Inventory Stats Exit!");
+            }
+        }
         //invetory 
         //item
         //stat
-        //
+        //info
     }
 }
